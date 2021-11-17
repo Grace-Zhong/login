623874f9975e3132cf25aa7251fe16b80a6aaafe
Grace <250467347@qq.com>
2021-11-17 14:32:16 +1030

    feat: Add type for menu item

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import { Button, Toolbar } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import IMenuItem from '../../common/Interfaces/IMenuItem';
 import menuData from './menuData.json';
 
 const Header = () => {
+  // const renderMenuBtn = ()
+
   return (
     <Toolbar>
-      {menuData.map((item) => (
+      {menuData.map((item:IMenuItem) => (
         <Button key={item.id}>
           <Link to={item.path}>
             {item.name}
